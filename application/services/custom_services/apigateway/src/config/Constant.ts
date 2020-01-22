a07a50bb99040147640218ca038bf0be97cbc138
Arul17 <arul.cornelious@10decoders>
2020-01-22 14:46:21 +0530

    code commited from geppetto!

---
+++ application/services/custom_services/apigateway/src/config/Constant.ts
@@ -0,0 +1,4 @@
+export const SECURITYURL = 'http://arul-app.arul.svc.cluster.local:3007';
+export const AUTHPROXYURL = 'http://arul-app.arul.svc.cluster.local:3009';
+export const ADMINURL = 'http://arul-app.arul.svc.cluster.local:3010';
+export const CAMUNDAURL = 'http://arul-app.arul.svc.cluster.local:3008';
